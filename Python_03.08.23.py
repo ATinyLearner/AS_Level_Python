10fe2c612accc9475b6383e32261644f523b9588
ATinyLearner <avik423@outlook.com>
2023-08-03 10:03:47 +0530

    Python Class 03.08.23 added

---
+++ Python_03.08.23.py
@@ -0,0 +1,29 @@
+# uses of print
+# print("Avik Ghosh") # print helps us printing
+# print(23)
+# print(f"1+1={1+1}")
+
+# if else
+
+# if(): #conditions
+#     pass # if condition follows
+
+# Ramu's example
+# Ramu is very bad student. He does not score well in class
+# Ramu want ps5, he goes to his parents
+
+# two logical operators 
+# and : and is used when we want to follow both conditions
+# or : or is used when we want to follow only one condition among two
+
+ramu_marks=70
+
+if(ramu_marks>=90):
+    print("Ramu will get a ps5")
+elif(ramu_marks>=70 and ramu_marks<90):
+    print("Ramu will get a new shoe!")
+else:
+    print("Ramu will be beaten with slippers!")
+
+
+
---
+++ test.py
@@ -0,0 +1,13 @@
+
+ramu_marks=35
+
+if(ramu_marks>=90):
+    print("Ramu will get a ps5!")
+elif(ramu_marks>=70 and ramu_marks<90):
+    print("Ramu will get a new shoe!")
+elif(ramu_marks>=50 and ramu_marks<70):
+    print("Ramu will not get anything!")
+elif(ramu_marks>=30 and ramu_marks<50):
+    print("Ramu is not going to eat for 10 days!")
+else:
+    print("Ramu is not going to come home!")
