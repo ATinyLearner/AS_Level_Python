f4311cb5340b88c632f17f604b90370c8409018a
ATinyLearner <avik423@outlook.com>
2023-08-04 10:02:53 +0530

    Python 04.08.23 is added

---
+++ Python_04.08.23.py
@@ -0,0 +1,37 @@
+# variable naming
+studentName="Oorjit"
+student_name="Surya"
+
+#data types
+int # integer ,1,3,5,6
+float # float ,0.3,6.79
+str # string is text, I am a student.
+
+#type()  #detects type of data
+student_number=50
+print(type(99.03))
+print(type(student_name))
+print(type(student_number))
+
+
+#logical operators and comparison operators
+# > , < , and, or , == ,>=,<=
+# > or equal >=, < or equal <=
+
+print(student_name=="Surya")
+
+#Arithmetic operators
+# + , - , *,/,//,%
+print(15/2)
+print(15//2)
+print(15%2)
+
+# a number is even or odd
+number=14
+if(number%2==0): # checking even condition
+    print(f"{number} is a even number!")
+else:
+    print(f"{number} is a odd number!")
+
+
+# put odd condition using if and solve it as homework.
